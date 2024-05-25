6164deab9aeaceb235e3363a59851a0331a9305e
MuzammilShah47 <muzammilshah47rgc@gmail.com>
2024-05-25 22:49:14 +0500

    new info added

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import requests
+
+payload = {
+    'v': 1,
+    'tid': 'G-2KQHVE1M1J',  # Replace with your Tracking ID
+    'cid': '555',  # Client ID, can be a unique identifier for your users
+    't': 'pageview',
+    'dp': '/home',  # Page path
+}
+
+response = requests.post("http://www.google-analytics.com/collect", data=payload)
+
+if response.ok:
+    print('Event sent successfully')
+else:
+    print('Failed to send event')
+
 app = Flask('__name__')
 model=pickle.load(open('model.pkl','rb'))
 
